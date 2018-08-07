54cce694449d1c9928075bd3b21ce28caea98c1a
TianLi <tianli@TianLideiMac.local>
2018-08-07 11:14:44 +0800

    Initial Commit

---
+++ 自定义弹出提醒框/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义弹出提醒框
+//
+//  Created by TianLi on 2018/8/7.
+//  Copyright © 2018年 TianLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
